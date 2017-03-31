904afdc9fa7c41ba5a0bb50b7bbe1c37ff64997f
dongdongzhang <dongdongzhang@ecarx.com.cn>
2017-03-31 19:11:50 +0800

    get and show ffmlibs info
    Signed-off-by: dongdongzhang <dongdongzhang@ecarx.com.cn>

--- FFmpegLib/app/src/main/cpp/native-lib.cpp
+++ FFmpegLib/app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,129 @@
 #include <jni.h>
 #include <string>
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/avfilter.h>
 
-extern "C"
+
+
+
+
 JNIEXPORT jstring JNICALL
-Java_com_zdd_ffmpeglib_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+Java_com_zdd_ffmpeglib_MainActivity_stringFromJNI(JNIEnv *env, jobject instance) {
+
+    // TODO
+    const char *info = avcodec_configuration();
+
+    return (*env).NewStringUTF(info);
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_zdd_ffmpeglib_MainActivity_urlprotocolinfo(JNIEnv *env, jobject instance) {
+
+    char info[40000]={0};
+    av_register_all();
+
+    struct URLProtocol *pup = NULL;
+    //Input
+    struct URLProtocol **p_temp = &pup;
+    avio_enum_protocols((void **)p_temp, 0);
+    while ((*p_temp) != NULL){
+        sprintf(info, "%s[In ][%10s]\n", info, avio_enum_protocols((void **)p_temp, 0));
+    }
+    pup = NULL;
+    //Output
+    avio_enum_protocols((void **)p_temp, 1);
+    while ((*p_temp) != NULL){
+        sprintf(info, "%s[Out][%10s]\n", info, avio_enum_protocols((void **)p_temp, 1));
+    }
+
+    //LOGE("%s", info);
+    return (*env).NewStringUTF(info);
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_zdd_ffmpeglib_MainActivity_avformatinfo(JNIEnv *env, jobject instance) {
+
+    char info[40000] = { 0 };
+
+    av_register_all();
+
+    AVInputFormat *if_temp = av_iformat_next(NULL);
+    AVOutputFormat *of_temp = av_oformat_next(NULL);
+    //Input
+    while(if_temp!=NULL){
+        sprintf(info, "%s[In ][%10s]\n", info, if_temp->name);
+        if_temp=if_temp->next;
+    }
+    //Output
+    while (of_temp != NULL){
+        sprintf(info, "%s[Out][%10s]\n", info, of_temp->name);
+        of_temp = of_temp->next;
+    }
+    //LOGE("%s", info);
+    return (*env).NewStringUTF(info);
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_zdd_ffmpeglib_MainActivity_avcodecinfo(JNIEnv *env, jobject instance) {
+
+    char info[40000] = { 0 };
+
+    av_register_all();
+
+    AVCodec *c_temp = av_codec_next(NULL);
+
+    while(c_temp!=NULL){
+        if (c_temp->decode!=NULL){
+            sprintf(info, "%s[Dec]", info);
+        }
+        else{
+            sprintf(info, "%s[Enc]", info);
+        }
+        switch (c_temp->type){
+            case AVMEDIA_TYPE_VIDEO:
+                sprintf(info, "%s[Video]", info);
+                break;
+            case AVMEDIA_TYPE_AUDIO:
+                sprintf(info, "%s[Audio]", info);
+                break;
+            default:
+                sprintf(info, "%s[Other]", info);
+                break;
+        }
+        sprintf(info, "%s[%10s]\n", info, c_temp->name);
+
+
+        c_temp=c_temp->next;
+    }
+    //LOGE("%s", info);
+    return (*env).NewStringUTF(info);
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_zdd_ffmpeglib_MainActivity_avfilterinfo(JNIEnv *env, jobject instance) {
+
+    char info[40000] = { 0 };
+    avfilter_register_all();
+    AVFilter *f_temp = (AVFilter *)avfilter_next(NULL);
+    while (f_temp != NULL){
+        sprintf(info, "%s[%10s]\n", info, f_temp->name);
+    }
+    //LOGE("%s", info);
+    return (*env).NewStringUTF(info);
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_zdd_ffmpeglib_MainActivity_configurationinfo(JNIEnv *env, jobject instance) {
+
+    char info[10000] = { 0 };
+    av_register_all();
+
+    sprintf(info, "%s\n", avcodec_configuration());
+
+    //LOGE("%s", info);
+    return (*env).NewStringUTF(info);
+}
+
 }
--- FFmpegLib/app/src/main/java/com/zdd/ffmpeglib/MainActivity.java
+++ FFmpegLib/app/src/main/java/com/zdd/ffmpeglib/MainActivity.java
@@ -1,8 +1,30 @@
 package com.zdd.ffmpeglib;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * @CreateDate: 2017/3/31 下午4:59
+ * @Author: lucky
+ * @Description:
+ * @Version: [v1.0]
+ * <p>
+ * <p>
+ * 本程序是移植FFmpeg到安卓平台的最简单程序。它可以打印出FFmpeg类库的下列信息：
+ * Protocol:  FFmpeg类库支持的协议
+ * AVFormat:  FFmpeg类库支持的封装格式
+ * AVCodec:   FFmpeg类库支持的编解码器
+ * AVFilter:  FFmpeg类库支持的滤镜
+ * Configure: FFmpeg类库的配置信息
+ */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,14 +33,29 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    @BindView(R.id.btn_configuration)
+    Button mBtnConfiguration;
+    @BindView(R.id.btn_protocol)
+    Button mBtnProtocol;
+    @BindView(R.id.btn_format)
+    Button mBtnFormat;
+    @BindView(R.id.btn_codec)
+    Button mBtnCodec;
+    @BindView(R.id.btn_filter)
+    Button mBtnFilter;
+    @BindView(R.id.tv_info)
+    TextView mTvInfo;
+    @BindView(R.id.activity_main)
+    LinearLayout mActivityMain;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+
     }
 
     /**
@@ -26,4 +63,36 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    public native String urlprotocolinfo();
+
+    public native String avformatinfo();
+
+    public native String avcodecinfo();
+
+    public native String avfilterinfo();
+
+    public native String configurationinfo();
+
+
+    @OnClick({R.id.btn_configuration, R.id.btn_protocol, R.id.btn_format, R.id.btn_codec, R.id.btn_filter})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_configuration:
+                mTvInfo.setText(configurationinfo());
+                break;
+            case R.id.btn_protocol:
+                mTvInfo.setText(urlprotocolinfo());
+                break;
+            case R.id.btn_format:
+                mTvInfo.setText(avformatinfo());
+                break;
+            case R.id.btn_codec:
+                mTvInfo.setText(avcodecinfo());
+                break;
+            case R.id.btn_filter:
+                mTvInfo.setText(avfilterinfo());
+                break;
+        }
+    }
 }
